44da5ecc17b7974d96ba3c0d56acbf13ffba9837
Alex G. Surkov <pipecregistracia@gmail.com>
2018-05-10 15:33:48 +0300

    fix first migration

--- migrations/sqls/20180217173548-create-users-up.sql
+++ migrations/sqls/20180217173548-create-users-up.sql
@@ -3,7 +3,7 @@ drop schema public cascade;
 create schema public;
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
+--SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
